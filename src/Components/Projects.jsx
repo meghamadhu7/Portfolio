bd2150b91d692250a7843987235994bf3c3e9107
meghamadhu7 <161140312+meghamadhu7@users.noreply.github.com>
2024-11-23 00:58:19 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+
 import {Container,Col,Row} from 'react-bootstrap';
-import projImg1 from '../assets/images/project-img1.png';
-import projImg2 from '../assets/images/project-img2.png';
-import projImg3 from '../assets/images/project-img3.png';
+import projectimg1 from '../Asserts/images/projectimg1.png';
+import projectimg2 from '../Asserts/images/projectimg2.png';
+import projectimg3 from '../Asserts/images/projectimg3.png';
 
 function Projects() {
-    const projects =[
+    const Projects =[
         {
-            title:"Spice Symph Restaurant",
+            title:" Restaurant",
             description: "Design & Development",
-            imgUrl:projImg1,
+            imgUrl:projectimg1,
         },
         {
-            title:"Spice Symph Restaurant",
+            title:" Restaur",
             description: "Design & Development",
-            imgUrl:projImg2,
+            imgUrl:projectimg2,
         },
         {
-            title:"Spice Symph Restaurant",
+            title:"Spice ",
             description: "Design & Development",
-            imgUrl:projImg3,
+            imgUrl:projectimg3,
         },
 
     ];
@@ -28,22 +29,21 @@ function Projects() {
     <section className="project" id="projects">
         <Container className='project-bx'>
          <h2>Projects</h2>
-          <p>To leverage my expertise in HTML, CSS, and JavaScript alongside my proficiency in modern frontend frameworks to create engaging and intuitive user experiences. Seeking opportunities to contribute to innovative projects while continually expanding my skill set and staying of industry trends.</p>
+          <p className='project-descripition'>To leverage my expertise in HTML, CSS, and JavaScript alongside my proficiency in modern frontend frameworks to create engaging and intuitive user experiences. Seeking opportunities to contribute to innovative projects while continually expanding my skill set and staying of industry trends.</p>
             <Row>
-                <Col>
-                    <a href="https://rakesh-2601.github.io/SpiceSymph_Restaurant/"><img src={projImg1} alt="Broken Image" width={'320px'} height={'200px'}/></a>
-                    <br /><br />
-                    <a href="https://rakesh-2601.github.io/SpiceSymph_Restaurant/" className='proj1'><h5>Spice Symph Restaurant</h5></a>
-                </Col>
-                <Col>
-                    <a href="https://rakesh-2601.github.io/Portfolio/"><img src={projImg2} alt="Broken Image" width={'320px'} height={'200px'}/></a>
-                    <br /><br />
-                    <a href="https://rakesh-2601.github.io/Portfolio/" className='proj2'><h5 >Portfolio</h5></a>
-                </Col>
-                <Col>
-                    <a href="https://rakesh-2601.github.io/IT_Services_Project/"><img src={projImg3} alt="Broken Image" width={'320px'} height={'200px'}/></a>
-                    <br /><br />
-                    <a href="https://rakesh-2601.github.io/IT_Services_Project/" className='proj3'><h5>IT Services</h5></a>
+                <Col className='project-list'>
+                    <a href="#"  className='proj'><img src={projectimg1} alt="Broken Image" width={'320px'} height={'200px'}/><h5>Godaddy</h5></a>
+              
+               
+              
+                    <a href="#" className='proj'><img src={projectimg2} alt="Broken Image" width={'320px'} height={'200px'}/><h5>PortMirror</h5></a>
+                   
+
+                
+              
+                    <a href="#" className='proj'><img src={projectimg3} alt="Broken Image" width={'320px'} height={'200px'}/>Portfoli</a>
+                   
+
                 </Col>
             </Row>
         </Container>
